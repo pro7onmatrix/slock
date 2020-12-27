572ddd5b8cd87336ce88014452b000aa7082c03a
professorchaos97 <yalepc@cup.uni-muenchen.de>
2020-12-27 12:56:22 +0100

    message

--- config.def.h
+++ config.def.h
@@ -10,3 +10,12 @@ static const char *colorname[NUMCOLS] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* default message */
+static const char * message = "Logged in as $USER, locked at $(date '+%a %b %d %H:%M')";
+
+/* text color */
+static const char * text_color = "#d8dee9";
+
+/* text size (must be a valid size) */
+static const char * font_name = "8x13";
